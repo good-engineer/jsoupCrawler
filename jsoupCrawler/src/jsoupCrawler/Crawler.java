b044b7765846c139b769eb586acdf514ceefde8d
good-engineer <ms_danesh@hotmail.com>
2020-05-27 00:42:54 +0900

    project initialized

---
+++ jsoupCrawler/src/jsoupCrawler/Crawler.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package jsoupCrawler;
+
+/**
+ * @author Maryam
+ *
+ */
+public class Crawler {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
